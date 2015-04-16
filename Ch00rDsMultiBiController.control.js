5e8b2e6ffe1e66f4a509ba1093d6f8d86fbfe15e
Sjoerd van Geffen <sjoerd.vangeffen@gmail.com>
2015-04-16 16:47:03 +0200

    Added configurable resolution for 7/14-bit CCs
    The resolution of 7/14-bit CCs can now be set to a lower value. For
    example, one may set the 14-bit CC resolution to 10001 (rather than the
    maximum resolution of 16384) for a better match to values displayed as
    percentages (0.00% to 100.00%).

--- Ch00rDsMultiBiController.control.js
+++ Ch00rDsMultiBiController.control.js
@@ -24,9 +24,15 @@
       LSB messages (as required per the MIDI specs), it may also be useful to somehow reset/cancel 
       such a timer function.
     
-    - To support touch-sensitive automation recording, AutomatableRangedValue.touch can perhaps somehow be used?
+    - To support touch-sensitive automation recording, AutomatableRangedValue.touch / 
+      addAutomationWriteModeObserver (callback) can perhaps somehow be used?
+      AutomatableRangedValue = UserControlBank.getControl()
+      AutomatableRangedValue = getParameter()
+      AutomatableRangedValue = getCommonParameter()
+      AutomatableRangedValue = getEnvelopeParameter()
     
-    - To support switching back to automation playback, AutomatableRangedValue.restoreAutomationControl() can perhaps somehow be used?
+    - To support switching back to automation playback, AutomatableRangedValue.restoreAutomationControl() / 
+      addAutomationOverrideObserver(callback) can perhaps somehow be used?
     
     - To support resetting mapped parameters to their default values, AutomatableRangedValue.reset	() can perhaps somehow be used?
     
@@ -34,15 +40,23 @@
 
 */
 
+loadAPI(1);
+
 // Some user-adjustable variables to define the ranges used for 14-bit CC pairs vs. plain 7-bit CCs
 // CCs 0/32 (= Bank Select MSB/LSB) are best avoided, just to be sure; YMMV
 var LOWEST_14bitMSB_CC = 0;
 var HIGHEST_14bitMSB_CC = 31;
+// Max. resolution for 14-bit CCs = 16384 steps (0-16383); can be set to lower value, e.g. 
+// set to 10001 for a better match to values displayed as percentages (0.00% to 100.00%)
+var RESOLUTION_14_BIT = 16384;
 // Make sure not to use overlapping CC number ranges for 14-bit and 7-bit! (prints warning to Controller Script Console)
 // Best avoid CC 0-63, reserving that range exclusively for 14-bit CC pairs 
 // CCs 120+ are best avoided completely as well, as these may easily cause issues; YMMV
 var LOWEST_7bit_CC = 64;
 var HIGHEST_7bit_CC = 119; 
+// Max. resolution for 7-bit CCs = 128 steps (0-127); can be set to lower value, e.g. 
+// set to 101 for a better match to values displayed as percentages (0.00% to 100%)
+var RESOLUTION_7_BIT = 128;
 
 // Enable/disable sending MIDI Beat Clock from Bitwig Studio to the output connected to the MIDI controller device
 // NB: may affect performance negatively - it is recommended to disable this if you don't need it; YMMV
@@ -60,9 +74,7 @@ var DEBUG = false;
 
 //  ----------------------------------------------------------------------------
 
-loadAPI(1);
-
-// Calculate array sizes
+// Calculate array sizes for storage of current and last previous value for each CC
 // 14-bit MSB/LSB CC pairs
 var LOWEST_14bitLSB_CC = LOWEST_14bitMSB_CC + 32;
 var HIGHEST_14bitLSB_CC = HIGHEST_14bitMSB_CC + 32;
@@ -76,6 +88,9 @@ if (DEBUG) println("LOWEST_7bit_CC = " + LOWEST_7bit_CC + " | HIGHEST_7bit_CC = 
 var ccValue = initArray(0, (LOW_RES_CC_RANGE * 16));
 var ccValueOld = initArray(0, (LOW_RES_CC_RANGE * 16));
 
+// var isShiftPressed = false;
+// var isResetPressed = false;
+
 host.defineController("Ch00rD", 
                       "Ch00rD's 7/14-bit CC Controller", 
                       "1.0", 
@@ -83,8 +98,8 @@ host.defineController("Ch00rD",
                       "Ch00rD"
                      );
 host.defineMidiPorts(1, 1);
-host.addDeviceNameBasedDiscoveryPair(["BCR 2000 Port 1"], ["BCR 2000 Port 1"]);
-host.addDeviceNameBasedDiscoveryPair(["BCR 2000 port 1"], ["BCR 2000 port 1"]);
+// host.addDeviceNameBasedDiscoveryPair(["BCR 2000 Port 1"], ["BCR 2000 Port 1"]);
+// host.addDeviceNameBasedDiscoveryPair(["BCR 2000 port 1"], ["BCR 2000 port 1"]);
 
 // A function to create an indexed function for the Observers
 function getValueObserverFunc(index, varToStore) {
@@ -139,7 +154,7 @@ function init() {
 */
 
     // Enable Poly AT translation into Timbre for the internal Bitwig Studio instruments
-//  MultiBi.assignPolyphonicAftertouchToExpression(0,   NoteExpression.TIMBRE_UP, 5);
+//  MultiBi.assignPolyphonicAftertouchToExpression(0,    NoteExpression.TIMBRE_UP, 5);
     MultiBi1.assignPolyphonicAftertouchToExpression(0,   NoteExpression.TIMBRE_UP, 5);
     MultiBi2.assignPolyphonicAftertouchToExpression(1,   NoteExpression.TIMBRE_UP, 5);
 /*
@@ -167,8 +182,8 @@ function init() {
     if (LOG_SYSEX)host.getMidiInPort(0).setSysexCallback(onSysex);
 
     // Make 14-bit CC pairs 1-31/33-63 plus 7-bit CCs 64-119 freely mappable for all 16 MIDI channels
-    userControls = host.createUserControls(((HIGHEST_14bitMSB_CC - LOWEST_14bitMSB_CC +1) + (HIGHEST_7bit_CC - LOWEST_7bit_CC + 1))*16);
     if (DEBUG) println("Creating " + (((HIGHEST_14bitMSB_CC - LOWEST_14bitMSB_CC +1) + (HIGHEST_7bit_CC - LOWEST_7bit_CC + 1))*16) + " User Control(s) and corresponding Value Observer(s) ... ");
+    userControls = host.createUserControls(((HIGHEST_14bitMSB_CC - LOWEST_14bitMSB_CC +1) + (HIGHEST_7bit_CC - LOWEST_7bit_CC + 1))*16);
     
     // 14-bit CC pairs
     for (var i = LOWEST_14bitMSB_CC; i <= HIGHEST_14bitMSB_CC; i++) {
@@ -179,7 +194,7 @@ function init() {
             // Set a label/name for each userControl
             userControls.getControl(c).setLabel("CC " + i + "+" + (i+32) + " (14-bit) MIDI Ch. " + j);
             // Add a ValueObserver for each userControl
-            userControls.getControl(c).addValueObserver(16383, getValueObserverFunc(c, ccPairValue));
+            userControls.getControl(c).addValueObserver(RESOLUTION_14_BIT, getValueObserverFunc(c, ccPairValue));
         }
     }
     
@@ -192,9 +207,10 @@ function init() {
             // Set a label/name for each userControl
             userControls.getControl(c).setLabel("CC " + i + " (7bit) MIDI Ch. " + j);
             // Add a ValueObserver for each userControl
-            userControls.getControl(c).addValueObserver(127, getValueObserverFunc(c, ccValue));
+            userControls.getControl(c).addValueObserver(RESOLUTION_7_BIT, getValueObserverFunc(c, ccValue));
         }
     }
+    
     if (DEBUG) println("---------- FINISHED INITIALIZATION ----------");
 }
 
@@ -245,7 +261,7 @@ function onMidi(status, data1, data2) {
         // Handle 7-bit CCs
         if (data1 >= LOWEST_7bit_CC && data1 <= HIGHEST_7bit_CC) {
             var index = (HIGH_RES_CC_RANGE * 16) + data1 - LOWEST_7bit_CC + (LOW_RES_CC_RANGE * MIDIChannel(status));
-            userControls.getControl(index).set(data2, 128);
+            userControls.getControl(index).set(data2, RESOLUTION_7_BIT);
             if (DEBUG) println("[7-bit] index = " + index);
         }
         // Handle 14-bit CC pairs
@@ -254,14 +270,15 @@ function onMidi(status, data1, data2) {
             var index = data1 - LOWEST_14bitMSB_CC + (HIGH_RES_CC_RANGE * MIDIChannel(status));
             // Get LSB from current value, add new MSB (= data2), store new 14-bit value
             var current_LSB = ccPairValue[index] & 0x7F;
-            var new_14bit_value = (data2 << 7) + current_LSB;
+//          var new_14bit_value = (data2 << 7) + current_LSB;
+            var new_14bit_value = (data2 << 7) | current_LSB; // bitwise operator may be slightly faster?
             ccPairValue[index] = ccPairValueOld[index] = new_14bit_value;
             // NB: The line below results is setting the value *twice* when a 14-bit CC pair is received;
             // It is necessary to support 7-bit CC messages (which may e.g. be useful to quickly swap between low and 
             // high resolution patches on the controller's end), however, if only strict 14-bit functionality is required,
             // it is recommended to comment it out, so the value is only set *after* a LSB message has been received
             // (including 'orphan' LSB messages, as required by the MIDI specs). 
-//             userControls.getControl(index).set(new_14bit_value, 16384);
+//             userControls.getControl(index).set(new_14bit_value, RESOLUTION_14_BIT);
             if (DEBUG) println("[14-bit MSB] index = " + index);
         }
         // LSB
@@ -269,9 +286,10 @@ function onMidi(status, data1, data2) {
             var index = data1 - LOWEST_14bitLSB_CC + (HIGH_RES_CC_RANGE * MIDIChannel(status));
             // Get MSB from current value, add new LSB (= data2), store new 14-bit value
             var current_MSB = (ccPairValue[index] >> 7) & 0x7F;
-            var new_14bit_value = (current_MSB << 7) + data2;
+//          var new_14bit_value = (current_MSB << 7) + data2;
+            var new_14bit_value = (current_MSB << 7) | data2; // bitwise operator may be slightly faster?
             ccPairValue[index] = ccPairValueOld[index] = new_14bit_value;
-            userControls.getControl(index).set(new_14bit_value, 16384);
+            userControls.getControl(index).set(new_14bit_value, RESOLUTION_14_BIT);
             if (DEBUG) println("[14-bit LSB] index = " + index);
         }
     }
