e746ef55eac0132d502fb0a80a988723c9a8fced
Sjoerd van Geffen <sjoerd.vangeffen@gmail.com>
2015-04-18 01:57:46 +0200

    minor change and fix

--- TrackMuteForNoteOutput/TrackMuteForNoteOutput.config.js
+++ TrackMuteForNoteOutput/TrackMuteForNoteOutput.config.js
@@ -7,13 +7,13 @@ Config.SETTINGS_INDEX_TRACKS_TOTAL = 0;
 // Total number of tracks (excluding master track and effect tracks) being observed; 
 // perhaps best to use a relatively large number, at least as large as the 
 // total number of tracks in (all) your projects
-Config.tracksTotal = 8;
+Config.tracksTotal = 24;
 
 Config.init = function ()
 {
     var prefs = host.getPreferences ();
 
-    Config.tracksTotalSetting = prefs.getNumberSetting ('Tracks', '(Maximum) number of supported tracks', 0, 1024, 1, '', 100);
+    Config.tracksTotalSetting = prefs.getNumberSetting ('Tracks', '(Maximum) number of supported tracks', 0, Config.tracksTotal, 1, '', Config.tracksTotal);
     Config.tracksTotalSetting.addRawValueObserver (function (value)
     {
         Config.tracksTotal = Math.floor (value);
